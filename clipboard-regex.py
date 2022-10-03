efbe8ba5975deed46b4de217094e9fbc5a4b50f3
pseudoacacial <aibanzai@protonmail.com>
2022-10-03 15:03:39 +0200

    optional border-radius

--- clipboard-regex.py
+++ clipboard-regex.py
@@ -2,8 +2,11 @@ import pyperclip as pc
 import re
 while (True):
     clipboard=pc.waitForNewPaste(timeout=None)
-    regex = re.search("left:\s*\d+px;\s*top:\s*\d+px;\s*width:\s*\d+px;\s*height:\s*\d+px;\s*", clipboard)
+    regex = re.search("(?:(border-radius:\s*\d+px;)[\s\S]*)?(left:\s*\d+px;\s*top:\s*\d+px;\s*width:\s*\d+px;\s*height:\s*\d+px;)\s*", clipboard)
     if regex:
-        newClipboard = regex.group().strip()
+        if(regex.group(1)):
+            newClipboard = '\n'.join(regex.groups()).strip()
+        else:
+            newClipboard = regex.group().strip()
         print(newClipboard)
         pc.copy(newClipboard)
