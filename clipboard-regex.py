b061bdb9676ce09685f4a7b62e8b95b0ca43e7e1
pseudoacacial <aibanzai@protonmail.com>
2022-10-04 13:45:32 +0200

    console shows current clipboard

--- clipboard-regex.py
+++ clipboard-regex.py
@@ -1,13 +1,24 @@
 import pyperclip as pc
+from os import system, name
 import re
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux
+    else:
+        _ = system('clear')
+
 while (True):
     clipboard=pc.waitForNewPaste(timeout=None)
     regex = re.search("(?:(border-radius:\s*\d+px;)[\s\S]*)?(left:\s*\d+px;\s*top:\s*\d+px;\s*width:\s*\d+px;\s*height:\s*\d+px;)\s*", clipboard)
     if regex:
         if(regex.group(1)):
-            newClipboard = '\n'.join(regex.groups())
+            clipboard = '\n'.join(regex.groups())
         else:
-            newClipboard = regex.group()
-        newClipboard = '\n'.join(' '.join(line.split()) for line in newClipboard.split('\n')).strip()
-        print(newClipboard)
-        pc.copy(newClipboard)
+            clipboard = regex.group()
+        clipboard = '\n'.join(' '.join(line.split()) for line in clipboard.split('\n')).strip()
+        pc.copy(clipboard)
+    clear()
+    print(clipboard)
