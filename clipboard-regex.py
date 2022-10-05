4548dd2080e73c3846db815c527bfa4f6f748de0
pseudoacacial <aibanzai@protonmail.com>
2022-10-05 13:22:34 +0200

    left and top can be less than 0

--- clipboard-regex.py
+++ clipboard-regex.py
@@ -12,7 +12,7 @@ def clear():
 
 while (True):
     clipboard=pc.waitForNewPaste(timeout=None)
-    regex = re.search("(?:(border-radius:\s*\d+px;)[\s\S]*)?(left:\s*\d+px;\s*top:\s*\d+px;\s*width:\s*\d+px;\s*height:\s*\d+px;)\s*", clipboard)
+    regex = re.search("(?:(border-radius:\s*\d+px;)[\s\S]*)?(left:\s*-?\d+px;\s*top:\s*-?\d+px;\s*width:\s*\d+px;\s*height:\s*\d+px;)\s*", clipboard)
     if regex:
         if(regex.group(1)):
             clipboard = '\n'.join(regex.groups())
